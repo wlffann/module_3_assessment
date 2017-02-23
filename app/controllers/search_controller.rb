42bd606a790cff101c7d16930d07dceea4c4ae7a
Ann Wolff <annwolffe@gmail.com>
2017-02-23 11:47:58 -0700

    Final refactor of BestBuyService

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,7 @@
 class SearchController < ApplicationController
   def index
-    response = BestBuyService.find_by_zipcode(params[:q]) 
+    response = BestBuyService.new.find_by_zipcode(params[:q]) 
     results_hash = JSON.parse(response.body) 
-    stores = results_hash['stores']
-    total = results_hash['total']
-    @results = ResultsPresenter.new(total, stores)
+    @results = ResultsPresenter.new(results_hash['total'], results_hash['stores'])
   end
 end
--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -1,9 +1,13 @@
 class BestBuyService
   include HTTParty
   base_uri 'api.bestbuy.com/v1'
+  
+  def initialize
+    @options = { query: { format: 'json', show: 'longName,storeType,phone,city,distance', apiKey: ENV['api_key'], pageSize: 10 } }
+  end
 
-  def self.find_by_zipcode(zipcode)
-    get("/stores(area(#{zipcode},25))?format=json&show=longName,storeType,phone,city,distance&apiKey=#{ENV['api_key']}&pageSize=10")   
+  def find_by_zipcode(zipcode)
+    self.class.get "/stores(area(#{zipcode},25))", @options
   end
 
 end
