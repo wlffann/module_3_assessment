ad0ea403b9ac2df9637c1ae9b0b30608cec69a06
Ann Wolff <annwolffe@gmail.com>
2017-02-23 11:01:33 -0700

    Finish item show and begin item delete

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -4,4 +4,21 @@ class Api::V1::ItemsController < ApplicationController
     render json: Item.all
   end
 
+  def show
+    render json: Item.find(params[:id])
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      render status: 204
+    else
+      render status: 500
+    end
+  end
+
 end
--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -4,6 +4,7 @@ describe 'Items API' do
   before do 
     create_list(:item, 5)
   end
+
   it 'GET /api/v1/items' do
     get '/api/v1/items'
     
@@ -18,5 +19,25 @@ describe 'Items API' do
     expect(item['image_url']).to eq(Item.first.image_url)
     expect(item).to_not have_key('created_at')
     expect(item).to_not have_key('updated_at')
+  end
+
+  it 'GET /api/v1/items/:id' do
+    get '/api/v1/items/1'
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(item['id']).to eq(Item.first.id)
+    expect(item['name']).to eq(Item.first.name)
+    expect(item['description']).to eq(Item.first.description)
+    expect(item['image_url']).to eq(Item.first.image_url)
+    expect(item).to_not have_key('created_at')
+    expect(item).to_not have_key('updated_at')
+  end
+
+  it 'DELETE /api/v1/items/:id' do
+    delete '/api/v1/items/1'
+
+    expect(response).to be(204)
   end
 end
